53b2c9394535bc7be68f0eecd31d77cfac0219ba
Bastian <Bastian.Mohr@student.fh-kiel.de>
2023-04-16 16:38:18 +0200

    filter um name stadtteil und typ ergänzt + sortierfunktion

--- entdecken.js
+++ entdecken.js
@@ -1,61 +1,115 @@
-
+let entdeckenListe;
+let filterliste;
 
 Papa.parse('listings.csv', {
-    download: true,
-    header: true,
-    skipEmptyLines: 'greedy',
-    complete: (results) => {
-      createEntdecken(results.data);
-    },
+  download: true,
+  header: true,
+  skipEmptyLines: 'greedy',
+  complete: (results) => {
+    entdeckenListe = results.data
+    filterliste = results.data
+    createEntdecken(results.data);
+  },
+});
+
+
+
+
+
+function filtern() {
+  let price = parseFloat(document.getElementById("maxPreis").value);
+  let stars = parseFloat(document.getElementById("rating").value);
+  let name = document.getElementById("name").value.toUpperCase()
+  let typ = document.getElementById("type").value.toUpperCase()
+  let Stadtteil = document.getElementById("stadtteil").value.toUpperCase()
+  let newlist = []
+  let data = entdeckenListe;
+
+  for (let i = 0; i < data.length; i++) {
+    priceString = data[i]["price"]
+    if (priceString !== undefined)
+      dataPrice = parseFloat(priceString.replace(/[^\d.]/g, ""));
+    dataStars = parseFloat(data[i]["review_scores_rating"])
+    if (data[i]["name"] !== undefined)
+      dataName = data[i]["name"].toUpperCase()
+    if (data[i]["property_type"] !== undefined)
+      dataTyp = data[i]["property_type"].toUpperCase()
+    if (data[i]["neighbourhood_cleansed"] !== undefined)
+      dataStadtteil = data[i]["neighbourhood_cleansed"].toUpperCase()
+
+
+    if (dataPrice <= price && dataStars >= stars && dataName.includes(name) && dataTyp.includes(typ) && dataStadtteil.includes(Stadtteil)) {
+      newlist.push(data[i])
+    }
+  }
+
+  newlist.sort(function (a, b) {
+    if (parseFloat(a.price.replace(/[^\d.]/g, "")) < parseFloat(b.price.replace(/[^\d.]/g, ""))) return -1;
+    if (parseFloat(a.price.replace(/[^\d.]/g, "")) > parseFloat(b.price.replace(/[^\d.]/g, ""))) return 1;
+    return 0;
   });
 
-function onFilter () {
-  Papa.parse('listings.csv', {
-      download: true,
-      header: true,
-      skipEmptyLines: 'greedy',
-      complete: (results) => {
-        filtern(results.data);
-      },
-    });
-}
-
-
-
-function filtern(data){
-  let price = parseInt(document.getElementById("maxPreis").value);
-  let stars = parseInt(document.getElementById("rating").value);
-  let newlist = []
-      for(let i=0 ;i<data.length;i++){
-          pricestring=data[i]["price"]
-          if(pricestring!==undefined)
-          dataprice=parseFloat(pricestring.replace(/[^\d.]/g, ""));
-          datastars=parseFloat(data[i]["review_scores_rating"])
-          
-
-          if(dataprice<=price && datastars>=stars){
-              newlist.push(data[i])
-          }
-      }
+  filterliste = newlist
   createEntdecken(newlist)
 }
 
+function aufsteigendPrice() {
+  filterliste.sort(function (a, b) {
+    if (a.price !== undefined && b.price !== undefined) {
+      if (parseFloat(a.price.replace(/[^\d.]/g, "")) < parseFloat(b.price.replace(/[^\d.]/g, ""))) return -1;
+      if (parseFloat(a.price.replace(/[^\d.]/g, "")) > parseFloat(b.price.replace(/[^\d.]/g, ""))) return 1;
+      return 0;
+    }
+  });
+  createEntdecken(filterliste)
+}
+
+function absteigendPrice() {
+  filterliste.sort(function (a, b) {
+    if (a.price !== undefined && b.price !== undefined) {
+      if (parseFloat(a.price.replace(/[^\d.]/g, "")) > parseFloat(b.price.replace(/[^\d.]/g, ""))) return -1;
+      if (parseFloat(a.price.replace(/[^\d.]/g, "")) < parseFloat(b.price.replace(/[^\d.]/g, ""))) return 1;
+      return 0;
+    }
+  });
+  createEntdecken(filterliste)
+}
 
 
-function createEntdecken (data) {
+function aufsteigendStars() {
+  filterliste.sort(function (a, b) {
+    if (a.review_scores_rating < b.review_scores_rating) return -1;
+    if (a.review_scores_rating > b.review_scores_rating) return 1;
+    return 0;
+  });
+  createEntdecken(filterliste)
+}
+
+function absteigendStars() {
+  filterliste.sort(function (a, b) {
+    if (a.review_scores_rating > b.review_scores_rating) return -1;
+    if (a.review_scores_rating < b.review_scores_rating) return 1;
+    return 0;
+  });
+  createEntdecken(filterliste)
+}
+
+
+
+function createEntdecken(data) {
   let anzeige = document.getElementById("FewoListe")
-  anzeige.innerHTML="";
-  for (let i = 0; i<100;i++){
-      let div = "<div class=\"colums1\">";
-      div += "<img src=\""+data[i]["picture_url"]+"\">";
-      div += "<h5>"+data[i]["name"]+"</h5>"
-      div += "<p>"+data[i]["room_type"]+"</p>"
-      div += "<p>"+data[i]["host_name"]+"</p>"
-      div += "<p>"+data[i]["price"]+"</p>"
-      div += "<span>"+data[i]["review_scores_rating"]+" &#9733;</span>"
-      div += "<p>"+data[i]["number_of_reviews"]+"</p>"
-      div += "</div> <br>"
-      anzeige.innerHTML += div
+  anzeige.innerHTML = "";
+  for (let i = 0; i < 100; i++) {
+    let div = "<div class=\"colums1\">";
+    div += "<img src=\"" + data[i]["picture_url"] + "\">";
+    div += "<h5>" + data[i]["name"] + "</h5>"
+    div += "<p>" + data[i]["room_type"] + "</p>"
+    div += "<p>" + data[i]["host_name"] + "</p>"
+    div += "<p>" + data[i]["price"] + "</p>"
+    div += "<span>" + data[i]["review_scores_rating"] + " &#9733;</span>"
+    div += "<p>" + data[i]["number_of_reviews"] + "</p>"
+    div += "</div> <br>"
+    anzeige.innerHTML += div
   }
 
 }
